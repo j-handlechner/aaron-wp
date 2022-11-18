045e441b355dd80c0a7d91aecdb9cb84059907d9
Johannes Handlechner <jhandlechner.mmt-b2021@fh-salzburg.ac.at>
2022-11-18 11:10:10 +0100

    add main.js to enqueue scripts

--- public/wp-content/themes/aarontheme/functions.php
+++ public/wp-content/themes/aarontheme/functions.php
@@ -2,6 +2,7 @@
 
 function add_theme_scripts_and_styles() {
 	wp_enqueue_style("style", get_stylesheet_uri());
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', true );
 	wp_enqueue_script( 'main-nav', get_template_directory_uri() . '/js/main-nav.js', array(), '1.0.0', true );
 }
 
