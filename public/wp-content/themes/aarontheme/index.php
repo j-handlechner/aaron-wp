77d7341773a0ecee915293679a13bd5723bd9025
Johannes Handlechner <jhandlechner.mmt-b2021@fh-salzburg.ac.at>
2022-12-29 22:29:26 +0100

    site now validates

--- public/wp-content/themes/aarontheme/index.php
+++ public/wp-content/themes/aarontheme/index.php
@@ -103,9 +103,7 @@
                                         <h3>
                                             <?php the_title(); ?>
                                         </h3>
-                                        <p>
-                                            <?php the_content(); ?>
-                                        </p>    
+                                        <?php the_content(); ?>   
                                     </div>
                                     <a href="#" class="button">Book Workshop</a>  
                                 
